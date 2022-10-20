dfdc6810cadaaedf02bbebdb378ea81e941c6b80
ESTIVAL Thomas <thomasestivaloc@gmail.com>
2022-10-20 19:55:50 +0200

    Projet terminé

--- script.js
+++ script.js
@@ -1,16 +1,44 @@
-const Q4 = document.querySelector(".Q4")
 const btn = document.querySelector("button")
 const radioButtons = document.querySelectorAll('input');
-let selectedSize = []
+const boxResult = document.querySelector(".score")
+const result = document.querySelector(".score__result")
+const articles = document.querySelectorAll("article")
 
-const goodResponses = ["Guido van Rossum", "Filtrer", "os", "pyweb"]
+let selectedResponses = []
+
+const correctionResponses = ["Guido van Rossum", "Modifier", "os", "Pyweb", "add"]
+
+let goodResponses = []
+
+let index = 0
 
 btn.addEventListener("click", (e) => {
-    
+    // Récupération des réponses utilisateur
     for (const radioButton of radioButtons) {
         if (radioButton.checked) {
-            selectedSize.push(radioButton.value);
+            selectedResponses.push(radioButton.value);
         }
     }
-    console.log(selectedSize);
+
+    // Correction des réponses
+    for (const userResponse of selectedResponses) {
+        index ++
+
+        if (correctionResponses.includes(userResponse)) {
+            goodResponses.push(userResponse)
+            document.querySelector(`.Q${index}`).className = "article-green"
+        }
+
+        else {
+            document.querySelector(`.Q${index}`).className = "article-red"
+        }
+    }
+
+    console.log(goodResponses);
+
+    boxResult.className = "score show"
+    result.innerText = `${goodResponses.length}/5`
+
+    articles.forEach()
+    
 })
